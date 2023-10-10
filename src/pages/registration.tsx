9646a2cefb7338961a3599eb2e3e401de573d4cb
Jhovani Gallardo Moreno <jhovanigallardo50@gmail.com>
2023-10-10 15:36:00 -0400

    added basic registration form with input fields

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -1,5 +1,14 @@
 export default function Registration() {
   return <div>
     <p>This will be the registration page</p>
+    <form>
+      <input type="text" placeholder="Email"></input>
+      <input type="text" placeholder="Username"></input>
+      <input type="text" placeholder="First Name"></input>
+      <input type="text" placeholder="Last Name"></input>
+      <input type="password" placeholder="password"></input>
+      <input type="password" placeholder="password"></input>
+      <button type="submit">Submit</button>
+    </form>
   </div>
 }
