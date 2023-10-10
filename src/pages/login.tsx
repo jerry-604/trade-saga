e5508fbabd1433621eb84483124a1a36ed5ee82f
Jhovani Gallardo Moreno <jhovanigallardo50@gmail.com>
2023-10-10 15:29:44 -0400

    Created login and registration pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,5 @@
+export default function Login() {
+  return <div>
+    <p>This will be the login page</p>
+  </div>
+}
---
+++ src/pages/registration.tsx
@@ -0,0 +1,5 @@
+export default function Registration() {
+  return <div>
+    <p>This will be the registration page</p>
+  </div>
+}
