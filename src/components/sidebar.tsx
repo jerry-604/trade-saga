91b47543549c187f524407265b0cae6312ebe39a
Zane Kleinberg <zaneskleinberg@gmail.com>
2023-10-10 21:40:51 -0400

    WIP sidebar and added pages

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,46 +1,61 @@
+import { useRouter } from 'next/router'
+import { FiHome, FiTrello, FiRss } from "react-icons/fi";
+import { BsFillQuestionCircleFill, BsFillArrowUpRightCircleFill } from "react-icons/bs";
+import Link from 'next/link'
+
 export default function Sidebar() {
+  const router = useRouter();
+  const currentRoute = router.pathname;
   return (
-    <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-white border-r-4 border-indigo-500">
-      <div className="text-black-100 text-xl">
-        <div className="p-2.5 mt-1 flex items-center">
-        
-          <h1 className="font-bold text-black-200 text-[15px] ml-3">
+    <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[350px] overflow-y-auto text-center bg-white border-r-2 border-[#EBEEF3] px-5 py-5 space-y-10">
+      <div className="text-[#424242]-100 text-xl">
+        <div className="h-[54px] p-2.5 mt-1 flex items-center justify-center">
+          <CircleWithT />
+          <h1 className="font-bold text-[#424242]-200 text-[18px] ml-3 text-[#424242]">
             TradeSaga
           </h1>
           <i className="bi bi-x cursor-pointer ml-28 lg:hidden"></i>
         </div>
       </div>
-      <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-black">
-        <i className="bi bi-house-door-fill"></i>
-        <span className="text-[15px] ml-4 text-black-200 font-bold">Home</span>
-      </div>
-      <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-black">
-        <i className="bi bi-bookmark-fill"></i>
-        <span className="text-[15px] ml-4 text-black-200 font-bold">
+      <Link href="/" className={`h-[54px] p-2.5 mt-3 flex items-center rounded-[10px] px-4 duration-300 cursor-pointer text-[#424242] ${currentRoute == "/" ? "bg-[#F4F6F8]" : null }`}>
+        <FiHome size="25px" />
+        <span className="text-[18px] ml-4 text-[#424242]-200 font-bold">Home</span>
+      </Link>
+      <Link href="/dashboard" className={`h-[54px] p-2.5 mt-3 flex items-center rounded-[10px] px-4 duration-300 cursor-pointer text-[#424242]  ${currentRoute == "/dashboard" ? "bg-[#F4F6F8]" : null }`}>
+      <FiTrello size="25px" />
+        <span className="text-[18px] ml-4 text-[#424242]-200 font-bold">
           Stock Dashboard
         </span>
-      </div>
-      <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-black">
-        <i className="bi bi-bookmark-fill"></i>
-        <span className="text-[15px] ml-4 text-black-200 font-bold">
+      </Link>
+      <Link href="/news" className={`h-[54px] p-2.5 mt-3 flex items-center rounded-[10px] px-4 duration-300 cursor-pointer text-[#424242]  ${currentRoute == "/news" ? "bg-[#F4F6F8]" : null }`}>
+      <FiRss size="25px" />
+        <span className="text-[18px] ml-4 text-[#424242]-200 font-bold">
           News
         </span>
-      </div>
-     <div className="absolute bottom-0 w-[300px]">
-     <div className="my-4 bg-gray-600 h-[1px]"></div>
-      <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-black">
-        <i className="bi bi-bookmark-fill"></i>
-        <span className="text-[15px] ml-4 text-black-200 font-bold">
+      </Link>
+     <div className="absolute bottom-[40px] w-[310px]">
+     <div className="my-4 bg-[#D9D9D9] h-[2px] w-[275px] ml-[17.5px]"></div>
+      <div className="p-2.5 mt-3 flex items-center rounded-[10px] px-4 duration-300 cursor-pointer text-[#424242]">
+      <BsFillQuestionCircleFill size="25px" color="black" />
+        <span className="text-[18px] ml-4 text-[#424242]-200 font-bold">
           Help
         </span>
       </div>
-      <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-black">
-        <i className="bi bi-bookmark-fill"></i>
-        <span className="text-[15px] ml-4 text-black-200 font-bold">
+      <div className="p-2.5 mt-3 flex items-center rounded-[10px] px-4 duration-300 cursor-pointer text-[#424242]">
+      <BsFillArrowUpRightCircleFill size="25px" color="black" />
+        <span className="text-[18px] ml-4 text-[#424242]-200 font-bold">
           Settings
         </span>
       </div>
       </div>
     </div>
+  );
+}
+
+const CircleWithT = () => {
+  return (
+      <div className="w-7 h-7 bg-black rounded-full flex items-center justify-center">
+          <span className="text-white font-semibold text-[15px]">T</span>
+      </div>
   );
 }
