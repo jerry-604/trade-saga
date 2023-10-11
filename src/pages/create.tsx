8cf869456a7dcc88db234d299a9fe1b19cc320cb
Zane Kleinberg <zaneskleinberg@gmail.com>
2023-10-10 22:01:29 -0400

    Added create game page

---
+++ src/pages/create.tsx
@@ -0,0 +1,12 @@
+export default function Create() {
+    return (   
+        <div className="min-h-screen flex bg-[#F5F5F5]">
+            <div className="flex-1 bg-white p-4">
+            </div>
+
+            <div className="flex-1 mix-blend-color-burn bg-[url('/create-background.png')] bg-cover bg-no-repeat bg-left p-4">
+            </div>
+        </div>
+    );
+  }
+  
