7cec48e9eb4e9e5a3a45db3cb6f7551e50431a5c
Jhovani Gallardo Moreno <jhovanigallardo50@gmail.com>
2023-10-23 17:05:22 -0400

    Created createUser API

--- src/server/routers/user-router.ts
+++ src/server/routers/user-router.ts
@@ -1,9 +1,34 @@
 import { publicProcedure, router } from '../trpc';
 import { z } from 'zod';
-import prisma from '../prisma'
+import prisma from '../prisma';
+import bcrypt from 'bcryptjs';
+
 export const userRouter = router({
   getUser: publicProcedure
     .query(async ({ ctx }) => {
       return await prisma.user.findMany();
     }),
+  createUser: publicProcedure.input(z.object({ name: z.string(), Fname: z.string(), Lname: z.string(), email: z.string(), password: z.string() })).mutation(async (opts) => {
+    const input = opts.input;
+
+    // Check if user already exists
+
+    // encrypt password
+
+    const result = await prisma.user.create({
+      data: {
+        name: input.name,
+        Fname: input.Fname,
+        Lname: input.Lname,
+        email: input.email,
+        password: input.password,
+        role: 'user',
+        dollars: 0,
+      }
+    });
+
+    console.log(result);
+
+    return result;
+  })
 });
