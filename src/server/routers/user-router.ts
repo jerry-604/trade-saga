62f165d85f6b036bd9c8f293be4b07fb9b397a5b
Jhovani Gallardo Moreno <jhovanigallardo50@gmail.com>
2023-10-24 14:34:10 -0400

    create new user input validation for empty fields and passwords not matching

--- src/server/routers/user-router.ts
+++ src/server/routers/user-router.ts
@@ -2,60 +2,75 @@ import { publicProcedure, router } from '../trpc';
 import { z } from 'zod';
 import prisma from '../prisma';
 import bcrypt from 'bcryptjs';
+import { signUp } from '@/src/utils/supabase';
+import { signIn } from '@/src/utils/supabase';
+import { TRPCError } from '@trpc/server';
 
 export const userRouter = router({
   getUser: publicProcedure
     .query(async ({ ctx }) => {
       return await prisma.user.findMany();
     }),
-  createUser: publicProcedure.input(z.object({ name: z.string(), Fname: z.string(), Lname: z.string(), email: z.string(), password: z.string() })).mutation(async (opts) => {
+  createUser: publicProcedure.input(z.object({ name: z.string(), Fname: z.string(), Lname: z.string(), email: z.string(), password: z.string(), confirmPassword: z.string() })).mutation(async (opts) => {
     const input = opts.input;
 
-    // Check if user already exists
-    const user = await prisma.user.findFirst({
-      where: {
-        OR: [
-          {
-            name: {
-              equals: input.name
-            }
-          },
-          {
-            email: {
-              equals: input.email
-            }
-          }
-        ]
-      },
-      select: {
-        id: true,
-        email: true,
-        name: true
+    for (const field of Object.values(input)) {
+      if (!field) {
+        throw new TRPCError({
+          code: 'UNPROCESSABLE_CONTENT',
+          message: "All fields are required",
+        });
       }
-    });
-
-    if (user) {
-      console.log("EMAIL OR USERNAME TAKEN");
-      return { error: `That ${user.name === input.name ? "username" : "email"} is already taken` };
     }
 
+    if (input.password !== input.confirmPassword) {
+      throw new TRPCError({
+        code: 'UNPROCESSABLE_CONTENT',
+        message: "Passwords do not match, please reconfirm your password",
+      });
+    }
+
+    // Check if user already exists
+    // const user = await prisma.user.findFirst({
+    //   where: {
+    //     OR: [
+    //       {
+    //         name: {
+    //           equals: input.name
+    //         }
+    //       },
+    //       {
+    //         email: {
+    //           equals: input.email
+    //         }
+    //       }
+    //     ]
+    //   },
+    // });
+
     // encrypt password
-    const hashedPassword = await bcrypt.hash(input.password, 10);
+    // const hashedPassword = await bcrypt.hash(input.password, 10);
 
-    const result = await prisma.user.create({
-      data: {
-        name: input.name,
-        Fname: input.Fname,
-        Lname: input.Lname,
-        email: input.email,
-        password: hashedPassword,
-        role: 'user',
-        dollars: 0,
-      }
-    });
+    // const result = await prisma.user.create({
+    //   data: {
+    //     name: input.name,
+    //     Fname: input.Fname,
+    //     Lname: input.Lname,
+    //     email: input.email,
+    //     password: hashedPassword,
+    //     role: 'user',
+    //     dollars: 0,
+    //   }
+    // });
 
-    console.log(result);
+    // console.log(result);
 
-    return result;
-  })
+    // const { data, error } = await signUp(input.email, input.password);
+    // if (error) {
+    //   console.error('Sign-up error:', error);
+    // } else {
+    //   console.log('Sign-up successful:', data);
+    // }
+    // return result;
+  }),
 });
