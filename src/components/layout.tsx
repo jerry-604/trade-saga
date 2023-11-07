3a57d4a5c1939894b52a550eb5220c6e857ef56f
Jeremiah Omolewa <83863055+jerry-604@users.noreply.github.com>
2023-11-07 02:47:14 -0500

    sidebar refactor and collapsible sidebar implementation

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,21 @@
+import React, { useState } from 'react';
 import Sidebar from './sidebar';
 
-export default function Layout({ children }: { children: any }) {
+export default function Layout({ children }) {
+  const [sidebarWidth, setSidebarWidth] = useState('300px'); // default sidebar width
+
+
+  const toggleSidebar = () => {
+    setSidebarWidth(currentWidth => currentWidth === '300px' ? '70px' : '300px'); // toggle between expanded and collapsed widths
+  };
+
   return (
-    <div className="flex h-screen">
-      <div className="flex-none w-[350px]">
-        <Sidebar />
+    <div className="flex h-screen bg-[#F5F7F9]">
+
+      <div className="flex-none" style={{  zIndex: 1000 }}>
+        <Sidebar onToggleSidebar={toggleSidebar} />
       </div>
-      <main className="flex-grow bg-[#F5F7F9] overflow-auto p-4">
+      <main className="flex-grow overflow-auto p-4" style={{ marginLeft: sidebarWidth }}>
         {children}
       </main>
     </div>
