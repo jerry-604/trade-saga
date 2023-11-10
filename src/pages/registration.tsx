d53c4f77e95cfdfabea2093c49526d53e79725f1
Matt <mmartin5@swarthmore.edu>
2023-11-10 11:37:00 -0500

    Finished Registration Page

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -49,41 +49,49 @@ export default function Registration() {
       });
   };
 
-  return <div className="h-screen relative">
-    <Image src="/create-background.png" height={1000} width={1000} alt="text" className="transform absolute top-0 left-0 w-screen h-screen z-0"></Image>
-    <div className="z-10 h-full">
+  return <div className="flex h-screen w-screen relative">
+    <Image src="/create-background.png" height={3000} width={3000} alt="text"
+     className="transform absolute top-0 left-0 w-screen h-screen"></Image>
+    <div className="flex h-full w-full">
       <form onSubmit={handleSubmit}>
-      <div className="fixed top-[120px] left-[550px] w-[600px] h-[700px] bg-white
-      rounded-tl-lg rounded-tr-lg rounded-bl-lg rounded-br-lg z-15"></div>
-      <p className="fixed top-[180px] left-[750px] text-xl bg-black-100 z-20">Create your Account</p>
-        <div className="fixed top-[250px] left-[645px] z-20">
-          <input onChange={handleNameChange} type="text" placeholder=" Username" 
-          className="w-[400px] h-[40px] bg-gray-200"></input>
-        </div>
-        <div className="fixed top-[320px] left-[645px] z-20">
-          <input onChange={handleEmailChange} type="text" placeholder=" Email"
-          className="w-[400px] h-[40px] bg-gray-200"></input>
-        </div>
-        <div className="absolute top-[390px] left-[645px] z-20">
-          <input onChange={handleFnameChange} type="text" placeholder=" First Name"
-          className="w-[400px] h-[40px] bg-gray-200"></input>
-        </div>
-        <div className="absolute top-[460px] left-[645px] z-20">
-          <input onChange={handleLnameChange} type="text" placeholder=" Last Name"
-          className="w-[400px] h-[40px] bg-gray-200"></input>
-        </div>
-        <div className="absolute top-[530px] left-[645px] z-20">
-          <input onChange={handlePasswordChange} type="password" placeholder=" Password"
-          className="w-[400px] h-[40px] bg-gray-200"></input>
-        </div>
-        <div className="absolute top-[600px] left-[645px] z-20">
-          <input onChange={handleConfirmPasswordChange} type="password" placeholder=" Password"
-          className="w-[400px] h-[40px] bg-gray-200"></input>
-        </div>
-        <div className="absolute top-[680px] left-[800px] w-[92px] h-[62px] bg-blue-700 rounded-tl-lg rounded-tr-lg rounded-bl-lg rounded-br-lg z-17"></div>
-        <div className="absolute top-[700px] left-[820px] z-20">
-          <button type="submit">Submit</button>
-          {error && <p>Error: {error}</p>}
+        <div className="flex items-center justify-center h-screen w-screen">
+          <div className="w-[600px] h-[700px] bg-white rounded-lg z-10">
+            <p className="flex justify-center mt-[50px] text-xl font-bold
+             text-shadow bg-black-100 z-20">Create your TradeSaga Account</p>
+            <div className="flex justify-center mt-[50px] z-20">
+              <input onChange={handleNameChange} type="text" placeholder="Username" 
+               className="w-[400px] h-[40px] rounded shadow-inner bg-gray-200 pl-[10px]"></input>
+            </div>
+            <div className="flex justify-center mt-[30px] z-20">
+              <input onChange={handleEmailChange} type="text" placeholder="Email"
+               className="w-[400px] h-[40px] rounded shadow-inner bg-gray-200 pl-[10px]"></input>
+            </div>
+            <div className="flex justify-center mt-[30px] z-20">
+              <input onChange={handleFnameChange} type="text" placeholder="First Name"
+               className="w-[400px] h-[40px] rounded shadow-inner bg-gray-200 pl-[10px]"></input>
+            </div>
+            <div className="flex justify-center mt-[30px] z-20">
+              <input onChange={handleLnameChange} type="text" placeholder="Last Name"
+               className="w-[400px] h-[40px] rounded shadow-inner bg-gray-200 pl-[10px]"></input>
+            </div>
+            <div className="flex justify-center mt-[30px] z-20">
+              <input onChange={handlePasswordChange} type="password" placeholder="Password"
+               className="w-[400px] h-[40px] rounded shadow-inner bg-gray-200 pl-[10px]"></input>
+            </div>
+            <div className="flex justify-center mt-[30px] z-20">
+              <input onChange={handleConfirmPasswordChange} type="password" placeholder="Confirm Password"
+               className="w-[400px] h-[40px] rounded shadow-inner bg-gray-200 pl-[10px]"></input>
+            </div>
+            <div className="flex justify-center mt-[55px] z-20">
+              <div className="w-[100px] h-[60px] bg-blue-600 shadow-lg rounded-lg">
+                <div className="flex items-center justify-center h-[60px] z-25">
+                  <button type="submit" className="flex items-center justify-center h-[60px] w-[100px] z-25
+                   font-bold text-white hover:rounded-lg hover:outline-black hover:outline">Submit</button>
+                </div>
+              </div>
+            </div>
+            {error && <p className="flex justify-center text-red-600 mt-[20px]">Error: {error}</p>}
+          </div>
         </div>
       </form>
     </div>
