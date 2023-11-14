9a2596a9b4fbee296eff7d307ed1aff674702d91
Zane Kleinberg <zaneskleinberg@gmail.com>
2023-11-14 14:26:41 -0500

    Add pie chart

--- src/components/games/game-analysis-page.tsx
+++ src/components/games/game-analysis-page.tsx
@@ -4,6 +4,7 @@ import { LoadingBoundary } from "../loading-boundary";
 import { trpc } from "../../utils/trpc";
 import GameUserInfo from "./game-user-info";
 import TickerTapeWidget from "./ticker-tape-widget";
+import { PieChart } from 'react-minimal-pie-chart';
 
 type Props = {
     user: any
@@ -11,7 +12,7 @@ type Props = {
     stockData: any
 }
 
-export default function GamePerformance({
+export default function GameAnalysis({
     user,
     gameData,
     stockData,
@@ -26,16 +27,26 @@ export default function GamePerformance({
                 return stock.symbol;
             })} />
             <div className="flex justify-between p-8">
-                <div className="sticky top-[400px]">
                 <GameUserInfo user={user} gameData={gameData} stockData={stockData} />
-                </div>
                 <div className="w-[50%] space-y-[20px]">
 
-                <div className="h-[400px] w-full bg-black">HELLO</div>
-                <div className="h-[400px]  w-full bg-black">HELOO</div>
+                    <div className="h-[400px] w-full bg-[#131313] rounded-[14px] p-6 content-start">
+                        <p className="text-[20px] font-semibold mb-1 text-[#FBFBFB]">Portolio Makeup</p>
+                        <PieChart
+                            data={[
+                                { title: 'One', value: 10, color: '#E38627' },
+                                { title: 'Two', value: 15, color: '#C13C37' },
+                                { title: 'Three', value: 20, color: '#6A2135' },
+                            ]}
+                            style={{ height: '100px', width: "100px" }}
+                            paddingAngle={5}
+                            lineWidth={15} 
+                        />
+                    </div>
+                    {/* <div className="h-[400px]  w-full bg-black">HELOO</div>
                 <div className="h-[400px]  w-full bg-black">HELLO</div>
                 <div className="h-[400px]  w-full bg-black">HELLO</div>
-                <div className="h-[400px]  w-full bg-black">HELLO</div>
+                <div className="h-[400px]  w-full bg-black">HELLO</div> */}
 
                 </div>
             </div>
