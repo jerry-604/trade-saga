e00863ca19c1977cecc2907afec772975ae9baa1
Jhovani Gallardo Moreno <jhovanigallardo50@gmail.com>
2023-11-27 12:13:57 -0500

    update password page basic form and handler

--- src/pages/updatePassword.tsx
+++ src/pages/updatePassword.tsx
@@ -1,7 +1,24 @@
+import { updatePassword } from "../utils/supabase";
+import { useState } from "react";
+
 export default function passwordReset() {
+  const [newPassword, setNewPassword] = useState('');
+  const [error, setError] = useState('');
+  const handleSubmit = async () => {
+    const { data, error } = await updatePassword(newPassword);
+    if (error) {
+      console.error(error);
+      setError(error.message);
+    } else {
+      console.log("successfully updated password");
+      // window.location.href = "/";
+    }
+  };
   return (
     <div>
-
+      <input type="password" placeholder="new password"></input>
+      <button onClick={handleSubmit}></button>
+      {error && <p>{error}</p>}
     </div>
   );
 }
