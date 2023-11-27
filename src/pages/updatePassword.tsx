6784f8720c73fecec807c9327208862dd2896e9c
Jhovani Gallardo Moreno <jhovanigallardo50@gmail.com>
2023-11-27 13:32:32 -0500

    styled and implemented form

--- src/pages/updatePassword.tsx
+++ src/pages/updatePassword.tsx
@@ -1,10 +1,19 @@
 import { updatePassword } from "../utils/supabase";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 import { useState } from "react";
 
 export default function passwordReset() {
   const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showCPassword, setShowCPassword] = useState(false);
+
   const handleSubmit = async () => {
+    if (newPassword !== confirmPassword) {
+      setError("passwords don't match");
+      return;
+    }
     const { data, error } = await updatePassword(newPassword);
     if (error) {
       console.error(error);
@@ -19,11 +28,31 @@ export default function passwordReset() {
     setNewPassword(e.target.value);
   };
 
+  const handleCPasswordChange = (e: any) => {
+    setConfirmPassword(e.target.value);
+  };
+
   return (
-    <div>
-      <input value={newPassword} onChange={handlePasswordChange} type="password" placeholder="new password"></input>
-      <button onClick={handleSubmit}>submit</button>
-      {error && <p>{error}</p>}
+    <div className="h-screen flex justify-center bg-[#F5F7F9]">
+      <div className="flex flex-col items-center place-self-center self-center border border-[#EBEEF3] border-solid rounded py-5 px-10 w-[450px] h-fit bg-white shadow-sm">
+        <h1 className="text-xl text-bold border-b border-[#EBEEF3] border-solid px-5 font-bold">Reset Password</h1>
+        <div className="w-full">
+          <p className="mt-3 font-semibold">New Password:</p>
+          <div className="flex">
+            <input value={newPassword} className="bg-[#F4F6F8] rounded shadow-inner pl-1 mr-1 w-full" onChange={handlePasswordChange} type={`${showPassword ? "text" : "password"}`}></input>
+            {showPassword ? <BsEyeFill size={"25px"} onClick={() => setShowPassword(!showPassword)} /> : <BsEyeSlashFill size={"25px"} onClick={() => setShowPassword(!showPassword)} />}
+          </div>
+          <p className="mt-3 font-semibold">Confirm Password:</p>
+          <div className="flex">
+            <input value={confirmPassword} className="bg-[#F4F6F8] rounded shadow-inner pl-1 mr-1 w-full" onChange={handleCPasswordChange} type={`${showCPassword ? "text" : "password"}`}></input>
+            {showCPassword ? <BsEyeFill size={"25px"} onClick={() => setShowCPassword(!showCPassword)} /> : <BsEyeSlashFill size={"25px"} onClick={() => setShowCPassword(!showCPassword)} />}
+          </div>
+          <div className="grid place-items-center">
+            <button className="bg-[#F4F6F8] rounded border border-[#EBEEF3] border-solid w-fit px-5 mt-5 place-self-center active:shadow-inner" onClick={handleSubmit}>Submit</button>
+            {error && <p className="mt-5 bg-red-100 rounded border border-red-200 border-solid w-fit place-self-center px-5">{error}</p>}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
