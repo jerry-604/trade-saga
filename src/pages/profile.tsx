c0c4e7824adefa75fc34aa0b17bd50dfd8bbab05
Matt <mmartin5@swarthmore.edu>
2023-11-27 21:33:41 -0500

    Finished profile settings page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getSession, signOut, uploadAvatar } from "../utils/supabase";
+import Link from 'next/link';
 import Image from "next/image";
 
 import { trpc } from "../utils/trpc";
@@ -50,6 +51,13 @@ export default function Profile() {
     }
   };
 
+  const truncateFileName = (fileName: string, maxLength: number) => {
+    if (fileName.length > maxLength) {
+      return fileName.substring(0, maxLength - 3) + '...';
+    }
+    return fileName;
+  };
+
   const handleImageChange = (event: any) => {
     setImage(event.target.files[0]);
   };
@@ -97,59 +105,73 @@ export default function Profile() {
 
   if (Object.keys(session).length === 0) {
     return (
-      <div>
-        <p>Please log in to see profile settings</p>
-        <button onClick={() => window.location.href = "/login"}>Login</button>
+      <div className="flex w-full h-full">
+        <div className="flex justify-center items-center h-screen w-screen">
+          <div className="w-[400px] h-[200px] bg-white rounded-lg shadow-lg">
 
-        <div>
-          <h2>[Other Settings Go Here Maybe Probably, if not we can redirect]</h2>
+            <p className="flex justify-center mt-[40px] font-bold">Please log in to see profile settings</p>
+            
+            <div className="flex justify-center h-[50px] w-[200px] bg-blue-600 mt-[30px] mx-auto
+            font-bold text-white rounded-lg hover:rounded-lg hover:outline-black hover:outline">
+              <button onClick={() => window.location.href = "/login"}>Click here to login</button>
+            </div>
+
+          </div>
         </div>
       </div>
     );
-    window.location.href = "/login";
+    
   } else {
     return <div className="flex w-full h-full">
       <div className="flex items-center justify-center h-screen w-screen">
         <div className="w-[500px] h-[700px] bg-white rounded-lg shadow-lg">
-          <p className="flex justify-center mt-[25px] text-xl font-bold
-            text-shadow bg-black-100">Profile Settings</p>
 
-          {/* @ts-ignore */}
+          <p className="flex justify-center mt-[25px] text-xl font-bold
+            text-shadow bg-black-100">Profile Settings Page</p>
+
           <Image src={user.data.imageUrl} width={100} height={100} alt="profile picture" 
           className="mx-auto mt-[10px]"/>
-      {Fname}
-      {Lname}
-
-      {JSON.stringify(user)}
-      {/* @ts-ignore */}
-      <p>Email: {JSON.stringify(session.user.email)}</p>
-      {error && <p>{JSON.stringify(error)}</p>}
-      <form onSubmit={handleImageUpload}>
-        <h2>Update Profile Picture</h2>
-
-          {/*JSON.stringify(user)*/}
-          {/* @ts-ignore */}
-          <p className="flex justify-center mt-[10px]">User email: {session.user.email}</p>
+          
+          <p className="flex justify-center mt-[10px] mb-[10px]">User email: {session.user.email}</p>
           {error && <p className="flex justify-center">{JSON.stringify(error)}</p>}
+
+          <div className="border-b border-[#EBEEF3] border-solid"></div>
 
           <form onSubmit={handleImageUpload}>
             <h2 className="flex justify-center underline mt-[10px]">Update Profile Picture</h2>
 
-          <div className="flex justify-center mt-[10px]">
-            <input
-              onChange={handleImageChange}
-              accept=".jpg, .png, .jpeg"
-              type="file"
-            ></input>
-          </div>
+            <div className="flex justify-center mt-[10px]">
+              <label className="flex justify-center items-center cursor-pointer bg-blue-600 font-bold
+               text-white rounded-lg h-[50px] w-[120px] hover:bg-blue-600 mb-[10px]
+               hover:rounded-lg hover:outline-black hover:outline">
+                Choose File
+                <input
+                  onChange={handleImageChange}
+                  accept=".jpg, .png, .jpeg"
+                  type="file"
+                  className="opacity-0 absolute inset-0 w-[50px] h-[120px] cursor-pointer"
+                ></input>
+              </label>
+            </div>
 
-          <div className="flex justify-center">
-            <input className="cursor-pointer w-[100px] h-[50px] bg-blue-600 rounded-lg mt-[10px] 
-            hover:rounded-lg hover:outline-black hover:outline font-bold text-white" type="submit" value="Upload" />
-          </div>
+            <div>
+              <p className="flex justify-center">
+                Selected File: 
+                <span className="overflow-hidden max-w-[230px]">
+                  {image ? truncateFileName(image.name, 20) : 'None'}
+                </span>
+              </p>
+            </div>
+
+            <div className="flex justify-center">
+              <input className="cursor-pointer w-[100px] h-[50px] bg-blue-600 rounded-lg mt-[10px] mb-[20px]
+              hover:rounded-lg hover:outline-black hover:outline font-bold text-white" type="submit" value="Upload" />
+            </div>
           </form>
+          
+          <div className="border-b border-[#EBEEF3] border-solid"></div>
 
-          <p className="flex justify-center mt-[20px] underline">Update Name</p>
+          <p className="flex justify-center mt-[10px] underline">Update Name</p>
           <div className="flex justify-center mt-[10px]">
             <input onChange={handleFNameChange} value={Fname} placeholder="First name"
             className="rounded shadow-inner bg-gray-200 pl-[10px] mr-[20px]"></input>
@@ -159,8 +181,23 @@ export default function Profile() {
           <div className="flex justify-center mt-[10px]">
             <button onClick={handleNameChanges}
             className="cursor-pointer w-[160px] h-[50px] bg-blue-600 rounded-lg mt-[10px] 
-            hover:rounded-lg hover:outline-black hover:outline font-bold text-white">Submit Changes</button>
+            hover:rounded-lg hover:outline-black hover:outline font-bold text-white mb-[20px]">Submit Changes</button>
           </div>
+
+          <div className="border-b border-[#EBEEF3] border-solid"></div>
+
+          <p className="flex justify-center mt-[10px] underline">Update Password</p>          
+          <div className="flex justify-center">
+            <Link href="/updatePassword">
+              <p className="flex justify-center items-center cursor-pointer
+                w-[280px] h-[50px] bg-blue-600
+                rounded-lg mt-[10px] hover:rounded-lg hover:outline-black
+                hover:outline font-bold text-white">
+                Click here to update Password
+              </p>
+            </Link>
+          </div>
+
         </div>
       </div>
     </div>;
