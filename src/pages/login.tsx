051643bde18204b08b5915f877e3e723b08770bb
Jhovani Gallardo Moreno <jhovanigallardo50@gmail.com>
2023-11-27 22:15:53 -0500

    redirect urls moved

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -26,7 +26,7 @@ export default function Login() {
       console.error('Sign-in error:', error);
     } else {
       console.log('Sign-in successful:', data);
-      window.location.href = "/dashboard";
+      window.location.href = "/home";
     }
   };
 
