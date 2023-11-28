03046a85b0c0851e0cd5a5434237edc0aee8970d
Jeremiah Omolewa <83863055+jerry-604@users.noreply.github.com>
2023-11-28 18:20:29 -0500

    landing page images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     optimizeFonts: false,
     images: {
-        domains: ['rqhpnffkqqqtowynnwxd.supabase.co','t3.ftcdn.net','placeimg.com','blog.hubspot.com','t0.gstatic.com'],
+    domains: ['rqhpnffkqqqtowynnwxd.supabase.co','t3.ftcdn.net','placeimg.com','blog.hubspot.com','t0.gstatic.com'],
     },
 };
 
